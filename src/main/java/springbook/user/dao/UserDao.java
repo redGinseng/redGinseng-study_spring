481c0f4a8280ca711fcc90f7f2f228338bbad561
sangwon.hong <hongsangwon90@gmail.com>
2023-06-15 19:25:20 +0900

    UserDao로부터 관계설정 책임을 분리

--- src/main/java/springbook/user/dao/UserDao.java
+++ src/main/java/springbook/user/dao/UserDao.java
@@ -15,12 +15,18 @@ import java.sql.SQLException;
 public class UserDao {
 
     private ConnectionMaker connectionMaker;
+    /*
+    DB Connection 관심사를 다루는 interface를 생성한 이유는, 인터페이스의 사용자(DUserDao)는 DB Connection 에 대해서는 신경을 끌 수 있게 해주기 위함이다.
+    인터페이스를 만들었다고는 하지만, UserDao를 사용하기 위해서는 여전히 conncetionMaker 구현체의 타입(DConnectionMaker)을 알아야 한다.
+    아직도 UserDao만 전해주고, 마음껏 DB Connect 을 구현하라고 할 수 없다.
+    UserDao 가 어떤 ConnectionMaker 클래스를 사용할지, 관계를 설정하는 관심사까지도 분리가 이뤄져야, 독립적으로 확장 가능한 클래스가 될 수 있다.
+    */
 
-    // DB Connection 관심사를 다루는 interface를 생성한 이유는, 인터페이스의 사용자(DUserDao)는 DB Connection 에 대해서는 신경을 끌 수 있게 해주기 위함이다.
-    // 인터페이스를 만들었다고는 하지만, UserDao를 사용하기 위해서는 여전히 conncetionMaker 구현체의 타입(DConnectionMaker)을 알아야 한다.
-    // 아직도 UserDao만 전해주고, 마음껏 DB Connect 을 구현하라고 할 수 없다.
-    public UserDao(){
-        connectionMaker = new DConnectionMaker();
+    // DConnectionMaker를 생성하는 코드는 UserDao와 ConncetionMaker 구현 클래스(DConnectionMaker)의 오브젝트 간 관계를 맺는 책임을 담당하는 코드였다
+    // 이것을 UserDao의 클라이언트(여기선 UserDadTest)에 넘겨버리자.
+    // 그리고 원래 관심사인 sql 생성, 실행에 집중한다.
+    public UserDao(ConnectionMaker connectionMaker){
+        this.connectionMaker = connectionMaker;
     }
 
     public void add(User user) throws ClassNotFoundException, SQLException {
--- src/test/java/springbook/user/dao/UserDaoTest.java
+++ src/test/java/springbook/user/dao/UserDaoTest.java
@@ -7,5 +7,14 @@ class UserDaoTest {
     public static void main(String[] args) {
         ConnectionMaker connectionMaker = new DConnectionMaker();
 
+        // UserDao 생성
+        // 사용할 ConnectionMaker 타입의 오브젝트 제공
+        // 두 오브젝트(UserDao, DConnectionMaker) 사이의 의존관계를 설정하는 효과
+        // 런타임에서 UserDaoTest 가 UserDao를 생성할 때 의존관계가 생성된다.
+        // 이렇게 UserDao에 있어서는 안됐던 관심사를 클라이언트로 떠넘기는 작업이 끝났다.
+        // 클라이언트 UserDaoTest 가 UserDao<->DConncetionMaker 간의 관계를 매핑해주는 덕분에
+        // D사는 자신들을 위한 DB접속클래스를 만들어서 UserDao를 사용할 수 있게 되었다.
+        UserDao dao = new UserDao(connectionMaker);
+
     }
 }
