2a2383022e672766a7eb64e154c2a6bc013f48d5
sangwon.hong <hongsangwon90@gmail.com>
2023-06-17 10:23:05 +0900

    Dao Factory 주석추가

--- src/main/java/springbook/user/dao/DaoFactory.java
+++ src/main/java/springbook/user/dao/DaoFactory.java
@@ -1,5 +1,10 @@
 package springbook.user.dao;
 
+/**
+ * DaoFactory는 오브젝트를 구성하고 관계를 정의하는 책임
+ * 새로운 ConnectionMaker 구현 클래스로 변경이 필요하면
+ * DaoFactory를 수정해서 변경된 클래스를 생성해 설정하도록 코드를 수정
+ */
 public class DaoFactory {
     public UserDao userDao() {
         ConnectionMaker connectionMaker = new DConnectionMaker();
