7f83b58c5d1345e191057970dc290b9e005423ba
sangwon.hong <hongsangwon90@gmail.com>
2023-06-17 18:41:53 +0900

    java.sql.DataSource 로 전환하고, counting 기능 유지

--- src/main/java/springbook/user/dao/DaoFactory.java
+++ src/main/java/springbook/user/dao/DaoFactory.java
@@ -1,36 +1,38 @@
 package springbook.user.dao;
 
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+
+import javax.sql.DataSource;
 
 /**
  * Application Context 를 적용한다.
  */
 
 @Configuration  //Application Context가 사용할 설정정보라는 표시, 사실 XML과 같은 설정정보라 봐도 무방한 클래스
-public class CountingDaoFactory {
+public class DaoFactory {
 
     @Bean   // UserDao 오브젝트 생성을 담당하는 IoC용 메소드라는 표시
     public UserDao userDao() {
         //의존관계 주입을 setter 방식으로 변경
         UserDao userDao = new UserDao();
-        userDao.setConnectionMaker(connectionMaker());
+        userDao.setDataSource(dataSource());
         return userDao;
     }
 
-    @Bean // ConncetionMaker 타입의 오브젝트를 생성해주는 메소드이므로 @Bean을 붙여준다
-    public ConnectionMaker connectionMaker() {
-        CountingConnectionMaker connectionMaker = new CountingConnectionMaker();
-        connectionMaker.setConnectionMaker(realConnectionMaker());
+    @Bean
+    public CountingAddedDataSource dataSource() {
+        CountingAddedDataSource dataSource = new CountingAddedDataSource();
+        dataSource.setDataSource(realDataSource());
 
-        return connectionMaker;
+        return dataSource;
     }
 
     @Bean
-    public ConnectionMaker realConnectionMaker() {
-        ConnectionMaker connectionMaker = new DConnectionMaker();
-
-        return connectionMaker;
+    public DataSource realDataSource() {
+        return new SimpleDriverDataSource();
     }
 
 
