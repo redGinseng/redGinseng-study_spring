b5bdc5014310ca9f3a4eba53fb20c84335bdb327
sangwon.hong <hongsangwon90@gmail.com>
2023-06-25 12:25:05 +0900

    예외처리

--- src/main/java/springbook/user/dao/UserDao.java
+++ src/main/java/springbook/user/dao/UserDao.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.sql.DataSource;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import springbook.user.DuplicateUserIdException;
 import springbook.user.domain.User;
 
 
@@ -32,11 +33,28 @@ public class UserDao {
             }
         };
 
+    // jdbcTemplate을 사용해서 간단하게
+//    public void add(User user) throws DuplicateUserIdException {
+//        try {
+//            this.jdbcTemplate.update("insert into users(id, name, password) value (?,?,?)",
+//                user.getId(), user.getName(), user.getPassword());
+//        } catch (SQLException e) {// 이미 근데 jdbcTemplate에서 DataAccessException (런타임 예외)으로 전환해주고 있다.
+//            if (e.getErrorCode() == MysqlErrorNumbers.ER_DUP_ENTRY) {
+//                // 밖에서 받을 때 컨트롤 할 수 없거나, 아무 의미 없는 SQLException(checked Exception) 이라면, unchecked Exception 으로 예외전환하자
+//                throw new DuplicateUserIdException(e);
+//            } else {
+//                throw new RuntimeException(e);
+//            }
+//        }
+//    }
+
 
     // jdbcTemplate을 사용해서 간단하게
-    public void add(User user) throws ClassNotFoundException, SQLException {
+    public void add(User user) throws DuplicateUserIdException {
+
         this.jdbcTemplate.update("insert into users(id, name, password) value (?,?,?)",
             user.getId(), user.getName(), user.getPassword());
+
     }
 
     // jdbcContext 클래스의 콜백을 사용하지말고, jdbcTemplate을 사용해서 내장콜백으로 만들자
