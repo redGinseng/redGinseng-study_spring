79a68529218264920dbb9e221d3e67251f1439ce
sangwon.hong <hongsangwon90@gmail.com>
2023-06-26 20:19:04 +0900

    User 에 필드 추가,  Enum 추가

--- src/main/java/springbook/user/dao/UserDaoJdbc.java
+++ src/main/java/springbook/user/dao/UserDaoJdbc.java
@@ -7,6 +7,7 @@ import javax.sql.DataSource;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import springbook.user.DuplicateUserIdException;
+import springbook.user.domain.Level;
 import springbook.user.domain.User;
 
 
@@ -30,6 +31,10 @@ public class UserDaoJdbc implements UserDao {
                 user.setId(rs.getString("id"));
                 user.setName(rs.getString("name"));
                 user.setPassword(rs.getString("password"));
+                user.setLevel(Level.valueOf(rs.getInt("level")));
+                user.setLogin(rs.getInt("login"));
+                user.setRecommend(rs.getInt("recommend"));
+
                 return user;
             }
         };
@@ -37,8 +42,8 @@ public class UserDaoJdbc implements UserDao {
     // jdbcTemplate을 사용해서 간단하게
     public void add(User user) throws DuplicateUserIdException {
 
-        this.jdbcTemplate.update("insert into users(id, name, password) value (?,?,?)",
-            user.getId(), user.getName(), user.getPassword());
+        this.jdbcTemplate.update("insert into users(id, name, password, level, login, recommend) value (?,?,?,?,?,?)",
+            user.getId(), user.getName(), user.getPassword(), user.getLevel().intValue(), user.getLogin(), user.getRecommend());
 
     }
 
